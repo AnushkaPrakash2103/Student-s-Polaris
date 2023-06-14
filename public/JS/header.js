791176f5954867d4601f2f5acf39f51a1c34cabf
AnushkaPrakash2103 <111519517+AnushkaPrakash2103@users.noreply.github.com>
2023-06-14 12:41:55 +0530

    Update and rename homepage.js to header.js

---
+++ public/JS/header.js
@@ -0,0 +1,12 @@
+
+function openNav() {
+  var x = document.getElementById("myTopnav");
+  x.classList.add("responsive");
+}
+
+function closeNav() {
+  var x = document.getElementById("myTopnav");
+  x.classList.remove("responsive");
+}
+
+
